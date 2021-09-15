c7b417826b35b96754c4b3cd46a9f2166725fd31
Dmitry <gilev.dimon@mail.ru>
2021-09-15 14:23:43 +0500

    Learning Project

---
+++ AutoFlashCollector/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AutoFlashCollector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.autoStartCheckBox = new System.Windows.Forms.CheckBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.panelCopying = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelCopying.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelInfo.Location = new System.Drawing.Point(13, 13);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(259, 24);
+            this.labelInfo.TabIndex = 0;
+            this.labelInfo.Text = "Состояние: Работает";
+            // 
+            // autoStartCheckBox
+            // 
+            this.autoStartCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.autoStartCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.autoStartCheckBox.Location = new System.Drawing.Point(8, 136);
+            this.autoStartCheckBox.Name = "autoStartCheckBox";
+            this.autoStartCheckBox.Size = new System.Drawing.Size(259, 24);
+            this.autoStartCheckBox.TabIndex = 1;
+            this.autoStartCheckBox.Text = "Запускать с Windows";
+            this.autoStartCheckBox.UseVisualStyleBackColor = true;
+            this.autoStartCheckBox.CheckedChanged += new System.EventHandler(this.autoStartCheckBox_CheckedChanged);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "USBCollector";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.progressBar.Location = new System.Drawing.Point(3, 37);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(261, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 2;
+            // 
+            // panelCopying
+            // 
+            this.panelCopying.Controls.Add(this.label2);
+            this.panelCopying.Controls.Add(this.label1);
+            this.panelCopying.Controls.Add(this.progressBar);
+            this.panelCopying.Location = new System.Drawing.Point(5, 40);
+            this.panelCopying.Name = "panelCopying";
+            this.panelCopying.Size = new System.Drawing.Size(267, 90);
+            this.panelCopying.TabIndex = 3;
+            this.panelCopying.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Копирование файлов...";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "1/1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.panelCopying);
+            this.Controls.Add(this.autoStartCheckBox);
+            this.Controls.Add(this.labelInfo);
+            this.MaximumSize = new System.Drawing.Size(300, 200);
+            this.MinimumSize = new System.Drawing.Size(300, 200);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Коллектор файлов";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.panelCopying.ResumeLayout(false);
+            this.panelCopying.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.CheckBox autoStartCheckBox;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Panel panelCopying;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AutoFlashCollector/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace AutoFlashCollector
+{
+    public partial class Form1 : Form
+    {
+        // Константа названия программы в реестре
+        const string appName = "AutoFlashCollector";
+
+        string usbPath = @"Z:\";
+        const string destPath = @"D:\Сортировочная папка\";
+        public bool isReaded = false;
+
+        private Thread capter;
+        private ManagementEventWatcher watcher;
+        private WqlEventQuery query;
+
+        // Текущее состояние работы программы
+        public enum _programState
+        {
+            None,
+            Activation,
+            Waiting,
+            Copying,
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.ShowInTaskbar = true;
+            notifyIcon.Visible = false;
+
+            // КароЧИ перехватываем и пытаем события подключения устройств
+            capter = new Thread(() =>
+            {
+                watcher = new ManagementEventWatcher();
+                query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2");
+                watcher.EventArrived += new EventArrivedEventHandler(watcher_EventArrived);
+                watcher.Query = query;
+                watcher.Start();
+            });
+            capter.Start();
+
+            autoStartCheckBox.Checked = Properties.Settings.Default.autoStart;
+        }
+
+        public void SetProgramState(_programState state)
+        {
+            switch(state)
+            {
+                case _programState.Waiting:
+                    labelInfo.Invoke(new Action(() => labelInfo.Text = "Состояние: Ожидание..."));
+                    break;
+                case _programState.Activation:
+                    labelInfo.Invoke(new Action(() => labelInfo.Text = "Состояние: Активация..."));
+                    break;
+                case _programState.Copying:
+                    labelInfo.Invoke(new Action(() => labelInfo.Text = "Состояние: Копирование..."));
+                    break;
+                case _programState.None:
+                    labelInfo.Invoke(new Action(() => labelInfo.Text = "Состояние: Ожидание..."));
+                    break;
+            }
+        }
+
+        // Установка в регистр автозапуска программы
+        public bool SetAutorunValue(bool autorun)
+        {
+            string ExePath = Application.ExecutablePath;
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autorun)
+                    reg.SetValue(appName, ExePath);
+                else
+                    reg.DeleteValue(appName);
+
+                Properties.Settings.Default.autoStart = autorun;
+                Properties.Settings.Default.Save();
+                reg.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // Действие при подключении флэщьки
+        private void watcher_EventArrived(object sender,
+        EventArrivedEventArgs e)
+        {
+            // >>-->>-->> АВТООПРЕДЕЛЕНИЕ ФЛЕШКИ СДЕЛАТЬ <<--<<--<<
+            List<string> drives = new List<string>();
+
+            foreach(DriveInfo di in DriveInfo.GetDrives())
+            {
+                drives.Add(di.Name);
+            }
+
+            usbPath = drives[drives.Count - 1];
+
+            if (!isReaded)
+                AskForCopyFiles();
+        }
+
+        // Возвращает количество всех файлов по пути
+        private int filesCountIn(string path)
+        {
+            int count = 0;
+
+            foreach(string dirPath in Directory.GetDirectories(path, "*", 
+                SearchOption.AllDirectories))
+            {
+                count++;
+            }
+            foreach (string newPath in Directory.GetFiles(usbPath, "*.*",
+                SearchOption.AllDirectories))
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        // Спрашиваем нужно ли копировать файлы?
+        private void AskForCopyFiles()
+        {
+            if (File.Exists(usbPath + "{auto}.txt"))
+            {
+                TakeFilesFromUSB();
+            }
+            else
+            {
+                DialogResult dr = MessageBox.Show($"Вы хотите скопировать все файлы с флешки тома {usbPath}?", "Подтверждение действия", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                    TakeFilesFromUSB();
+                }
+                else if (dr == DialogResult.No)
+                {
+                    // Nothing to DO
+                }
+            }
+        }
+
+        // Получаем информацию о флешке
+        private void InfoAboutDrive()
+        {
+            List<string> drives = new List<string>();
+
+            string totalStr = "";
+
+            foreach(DriveInfo di in DriveInfo.GetDrives())
+            {
+                drives.Add(di.Name + " - " + di.VolumeLabel);
+                totalStr += Environment.NewLine + di.Name + " - " + di.VolumeLabel;
+            }
+
+            MessageBox.Show(totalStr);
+        }
+
+        // Забираем файлы с флешки
+        private void TakeFilesFromUSB()
+        {
+            List<string> errorFiles = new List<string>();
+
+            isReaded = true;
+
+            SetProgramState(_programState.Copying);
+
+            // Считаем сколько всего файлов
+            int totalFiles = filesCountIn(usbPath);
+            int copyiedFiles = 0;
+
+            panelCopying.Invoke(new Action(() => panelCopying.Visible = true ));
+            progressBar.Invoke(new Action(() => progressBar.Maximum = totalFiles));
+
+
+            //Создать идентичную структуру папок
+            foreach (string dirPath in Directory.GetDirectories(usbPath, "*",
+                SearchOption.AllDirectories))
+            {
+                try
+                {
+                    Directory.CreateDirectory(dirPath.Replace(usbPath, destPath));
+                    progressBar.Invoke(new Action(() => progressBar.Value++));
+                    copyiedFiles++;
+                    label2.Invoke(new Action(() => label2.Text = copyiedFiles + "/" + totalFiles));
+                }
+                catch (Exception e)
+                {
+                    errorFiles.Add(dirPath);
+                }
+            }
+
+            // Копировать все файлы и перезаписать файлы с идентичным именем
+            foreach (string newPath in Directory.GetFiles(usbPath, "*.*",
+                SearchOption.AllDirectories))
+            {
+                try
+                {
+                    File.Copy(newPath, newPath.Replace(usbPath, destPath), true);
+                    progressBar.Invoke(new Action(() => progressBar.Value++));
+                    copyiedFiles++;
+                    label2.Invoke(new Action(() => label2.Text = copyiedFiles + "/" + totalFiles));
+                }
+                catch (Exception e)
+                {
+                    errorFiles.Add(newPath);                }
+            }
+
+            MessageBox.Show(copyiedFiles + " файлов и папок было успешно перемещено в " + destPath + "!");
+
+            if(errorFiles.Count > 0)
+            {
+                string str = "Ошибки в файлах: ";
+                for(int i = 0; i < errorFiles.Count; i++)
+                {
+                    str += Environment.NewLine + errorFiles[i];
+                }
+
+                MessageBox.Show(str);
+                errorFiles.Clear();
+            }
+
+            SetProgramState(_programState.Waiting);
+            panelCopying.Invoke(new Action(() => panelCopying.Visible = false));
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            capter.Abort();
+            watcher.Stop();
+        }
+
+        private void autoStartCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (autoStartCheckBox.Checked)
+            {
+                SetAutorunValue(true);
+            }
+            else
+            {
+                SetAutorunValue(false);
+            }
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                notifyIcon.Visible = false;
+            }
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            SetProgramState(_programState.Waiting);
+        }
+    }
+}
